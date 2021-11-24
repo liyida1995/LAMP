b7395c574e4796d4f01003bdb7ba99def64af836
liyida1995 <94302780+liyida1995@users.noreply.github.com>
2021-11-24 20:15:49 +0800

    Update util.h

--- util.h
+++ util.h
@@ -25,6 +25,60 @@ int Part1By1(int x)
 	return x;
 }
 
+int zfp1_compress (double * array, int nx, double tolerance, char filename[50]
+	      )
+{
+	zfp_type type;     /* array scalar type */
+	zfp_field* field;  /* array meta data */
+	zfp_stream* zfp;   /* compressed stream */
+	void* buffer;      /* storage for compressed stream */
+	size_t bufsize;    /* byte size of compressed buffer */
+	bitstream * stream; /* bit stream to write to or read from */
+	size_t zfpsize;    /* byte size of compressed stream */
+
+	/* allocate meta data for the 3D array a[nz][ny][nx] */
+	type = zfp_type_double;
+	field = zfp_field_1d (array, type, nx);
+
+	/* allocate meta data for a compressed stream */
+	zfp = zfp_stream_open (NULL);
+
+	/* set compression mode and parameters via one of three functions */
+	/*  zfp_stream_set_rate(zfp, rate, type, 3, 0); */
+	/* A_precision = zfp_stream_set_precision(zfp, precision, type);*/
+	zfp_stream_set_accuracy (zfp, tolerance);
+
+
+	/* allocate buffer for compressed data */
+	bufsize = zfp_stream_maximum_size (zfp, field);
+	buffer = malloc (bufsize);
+	assert (buffer);
+	/* associate bit stream with allocated buffer */
+	stream = stream_open (buffer, bufsize);
+	zfp_stream_set_bit_stream (zfp, stream);
+	zfp_stream_rewind (zfp);
+
+	/* compress array and output compressed stream */
+	//zfpsize = zfp_compress (zfp, field);
+	zfpsize = zfp_compress(zfp, field);
+	if (!zfpsize) {
+		fprintf(stderr, "compression failed\n");
+	}
+	else{
+		FILE *fp=fopen(filename,"w");
+
+		fwrite(buffer, 1, zfpsize, fp);
+		fclose(fp);
+	}
+	assert (zfpsize);
+	zfp_field_free (field);
+	zfp_stream_close (zfp);
+	stream_close (stream);
+
+	free( buffer);
+
+	return zfpsize;
+}
 
 // Inverse of Part1By1 - "delete" all odd-indexed bits
 int Compact1By1(int x)
