aadf0cdc7add660f8e53d13cb35ebb662db9aa79
liyida1995 <94302780+liyida1995@users.noreply.github.com>
2022-04-13 11:10:04 +0800

    Create parallel_LAMP_sz.sh

---
+++ scripts/parallel_LAMP_sz.sh
@@ -0,0 +1,31 @@
+#############################   LAMP   ####################################
+cd ./scripts
+rm ./sz_lamp.txt
+
+cd ../src
+sed -i "11s/.*/#define Level 9/" ./compression_LAMP.c
+sed -i "1s/.*/#define Level 9/" ./util.h
+make -f Makefile
+
+cd ../Data/Min
+../../src/compression_LAMP>>../../scripts/sz_lamp.txt
+cd ../../
+###########################################################################
+
+cd ./src
+sed -i "11s/.*/#define Level 5/" ./compression_LAMP.c
+sed -i "1s/.*/#define Level 5/" ./util.h
+make -f Makefile
+
+cd ../Data/Pin
+../../src/compression_LAMP>>../../scripts/sz_lamp.txt
+cd ../../
+###########################################################################
+
+cd ./src
+sed -i "11s/.*/#define Level 6/" ./compression_LAMP.c
+sed -i "1s/.*/#define Level 6/" ./util.h
+make -f Makefile
+
+cd ../Data/Green
+../../src/compression_LAMP>>../../scripts/sz_lamp.txt
