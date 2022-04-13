36706b6a288ecc50e9f98e54d7884ad9dd3c39c0
liyida1995 <94302780+liyida1995@users.noreply.github.com>
2022-04-13 11:08:31 +0800

    Update parallel_baseline_sz.sh

--- scripts/parallel_baseline_sz.sh
+++ scripts/parallel_baseline_sz.sh
@@ -1,1 +1,31 @@
+#############################   Baseline   ####################################
+cd ./scripts
+rm ./sz_baseline.txt
 
+cd ../src
+sed -i "11s/.*/#define Level 9/" ./compression_LAMP.c
+sed -i "1s/.*/#define Level 9/" ./util.h
+make -f Makefile
+
+cd ../Data/Min
+../../src/compression_LAMP>>../../scripts/sz_baseline.txt
+cd ../../
+###########################################################################
+
+cd ./src
+sed -i "11s/.*/#define Level 5/" ./compression_LAMP.c
+sed -i "1s/.*/#define Level 5/" ./util.h
+make -f Makefile
+
+cd ../Data/Pin
+../../src/compression_LAMP>>../../scripts/sz_baseline.txt
+cd ../../
+###########################################################################
+
+cd ./src
+sed -i "11s/.*/#define Level 6/" ./compression_LAMP.c
+sed -i "1s/.*/#define Level 6/" ./util.h
+make -f Makefile
+
+cd ../Data/Green
+../../src/compression_LAMP>>../../scripts/sz_baseline.txt
